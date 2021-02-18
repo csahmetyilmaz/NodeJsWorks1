7d02aa52bb2f944cc6acb37f26ba3f0548b6f588
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2021-02-18 16:27:43 +0100

    end of u1 commit

--- u1/intro.js
+++ u1/intro.js
@@ -1,6 +1,22 @@
     var http = require('http')
+    var log = require('./log.js')
+    var sayHello = require('./exportsDemo')
+
     http.createServer(function (request,response){
+        console.log(request.url)
+        //Request le alakali kisimlar
+        if(request.url=="/admin"){
+            response.write('<html><body><strong>Admin Sayfasi</strong></body></html>')
+        }else {
+            response.write('<html><body><strong>Anasayfa</strong></body></html>')
+        }
+
         //Client i bilgilendirmek icin baslik gonder
-        response.writeHead(200,{'Content-Type':'text/html'})
-        response.end('Merhaba') //burada mesaj yaziyoruz.
+        //response.writeHead(200,{'Content-Type':'text/html'})
+        response.end() //burada mesaj yaziyoruz.
     }).listen(8080)
+
+
+    log.information("<br>Sunucu yayina gecti")
+ console.log(sayHello());
+//    log.information(city);
